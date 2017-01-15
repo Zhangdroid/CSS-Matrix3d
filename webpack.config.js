2d51361651a580f1c8b183f1929f69d32ec99528
Zhuo Zhang <contrary.zhuo@gmail.com>
2017-01-15 21:32:15 +0800

    Init webpack config

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'matrix.min.js',
+    library: 'matrix',
+    libraryTarget: "umd"
+  },
+  module: {
+    rules: [
+      {test: /\.(js)$/, use: 'babel-loader'}
+    ]
+  }
+}
